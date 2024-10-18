39544d52984f0c03eb002b9b4c0e77907dbca686
ramakanth47 <105962313+ramakanth47@users.noreply.github.com>
2024-10-18 18:29:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import FormComponent from "./components/Formcomponent";
 
-//import img from "./images/background.jpg"
+import img from "./images/background.jpg"
 
 
 function App() {
   return (
     <div className="App">
       <div className="header">
-        {/* <img src={img} width="100%" height="20%" alt="background" /> */}
+         <img src={img} width="100%" height="20%" alt="background" /> 
       </div>
       <div className="form-section">
         <FormComponent />
